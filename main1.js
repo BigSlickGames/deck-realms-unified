ad1a9ea93340f32124960d9f9e3d91c5be9fed66
BigSlickGames <bigslickgames@gmail.com>
2025-11-20 06:07:40 +0530

    Updated main1.js

--- main1.js
+++ main1.js
@@ -1,4 +1,4 @@
-import { DeckManager } from "./DeckManager.js";
+import { DeckManager } from "./modules/DeckManager.js";
 import { DeckUI } from "../deck-realms-CMS/modules/DeckUI.js";
 import { MarketUI } from "../deck-realms-CMS/modules/MarketUI.js";
 
